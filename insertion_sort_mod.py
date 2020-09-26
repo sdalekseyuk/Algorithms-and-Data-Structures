febc0d709d535a60751965a6e0963e9594011a7f
Sergei Alekseiuk <sdalekseyuk@gmail.com>
2020-09-26 03:25:59 +0300

    Fixed indents

--- insertion_sort_mod.py
+++ insertion_sort_mod.py
@@ -2,7 +2,7 @@ def insertion_sort(lst):
     """Modified insertion sort"""
     for i in range(1, len(lst)):
         j = i - 1
-        while j >= 0 and lst[j] > lst[j+1]:
-            lst[j], lst[j+1] = lst[j+1], lst[j]
+        while j >= 0 and lst[j] > lst[j + 1]:
+            lst[j], lst[j + 1] = lst[j + 1], lst[j]
             j -= 1
     return lst
