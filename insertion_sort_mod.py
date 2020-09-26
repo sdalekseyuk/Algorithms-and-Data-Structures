21635d2496f8526c39b88cf28da848c07f5f8ec9
Sergei Alekseiuk <sdalekseyuk@gmail.com>
2020-09-26 03:21:21 +0300

    Initial commit

---
+++ insertion_sort_mod.py
@@ -0,0 +1,8 @@
+def insertion_sort(lst):
+    """Modified insertion sort"""
+    for i in range(1, len(lst)):
+        j = i - 1
+        while j >= 0 and lst[j] > lst[j+1]:
+            lst[j], lst[j+1] = lst[j+1], lst[j]
+            j -= 1
+    return lst
